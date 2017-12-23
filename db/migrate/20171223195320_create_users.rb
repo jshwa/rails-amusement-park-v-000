3e6d6633201b7db99a4850420c679536b93a92b2
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-23 20:14:06 +0000

    automatically backed up by learn

--- db/migrate/20171223195320_create_users.rb
+++ db/migrate/20171223195320_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name,
-      t.string :password-digest,
-      t.string :happiness,
-      t.string :nausea,
-      t.integer :height,
-      t.integer :tickets,
+      t.string :name
+      t.string :password-digest
+      t.string :happiness
+      t.string :nausea
+      t.integer :height
+      t.integer :tickets
       t.boolean :admin, default: false
 
       t.timestamps null: false
