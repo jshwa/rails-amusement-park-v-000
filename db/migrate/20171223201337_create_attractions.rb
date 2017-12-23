23aa670f12bd3778b6a3718c2624fb0d3fea8144
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-23 20:13:37 +0000

    automatically backed up by learn

--- db/migrate/20171223201337_create_attractions.rb
+++ db/migrate/20171223201337_create_attractions.rb
@@ -1,10 +1,10 @@
 class CreateAttractions < ActiveRecord::Migration
   def change
     create_table :attractions do |t|
-      t.string :name,
-      t.integer :min_height,
-      t.integer :nausea_rating,
-      t.integer :happiness_rating,
+      t.string :name
+      t.integer :min_height
+      t.integer :nausea_rating
+      t.integer :happiness_rating
       t.integer :ticket_number
     end
   end
