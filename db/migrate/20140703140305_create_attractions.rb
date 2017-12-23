8388d14451b843d1dab45a55832a48578fdcb97a
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-23 19:56:57 +0000

    automatically backed up by learn

--- db/migrate/20140703140305_create_attractions.rb
+++ db/migrate/20140703140305_create_attractions.rb
@@ -1,3 +1,10 @@
 class CreateAttractions < ActiveRecord::Migration
- # Write your migrations here
+  def change
+    create_table attractions do |t|
+      t.string :name,
+      t.integer :min_height,
+      t.integer :nausea_rating,
+      t.integer :happiness_rating
+      t.integer :ticket_number
+  end
 end
