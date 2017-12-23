f3ef3e3d4921dd7a2baaea0aa94b5e89bd7326f5
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-23 19:55:04 +0000

    automatically backed up by learn

---
+++ db/migrate/20171223195320_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password-digest
+      t.string :happiness
+      t.string :nausea
+      t.integer :height
+      t.integer :tickets
+
+      t.timestamps null: false
+    end
+  end
+end
