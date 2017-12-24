788860915a0e500457a290b0cac352014c9c03e2
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-24 05:20:25 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password_digest, :happiness, :nausea, :height, :tickets, :admin)
+    params.require(:user).permit(:name, :password, :happiness, :nausea, :height, :tickets, :admin)
   end
 end
