8959b4ae9b78d43392d03a1cce2c3cc86475f830
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-24 17:05:52 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-
-  get 'attractions/show'
-
-  get 'attractions/index'
-
-  get 'attractions/new'
-
+  
   get 'signin' => 'users#signin', as: :signin
   post 'signin' => 'sessions#create'
   get 'signout' => 'sessions#destroy', as: :signout
 
   root 'application#home'
   resources :users
+  resources :attractions
 
 end
