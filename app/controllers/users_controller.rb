20e484070449761cbea365f5232816666ddb1c68
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-24 04:58:03 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      redirect_to '/users/:id'
+      redirect_to user_path(user)
     else
       render 'new'
     end
