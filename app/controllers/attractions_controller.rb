ba9f8d0d627a0a6cb73cb0e2c009e85f0f58253e
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-24 19:05:44 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,4 +1,5 @@
 class AttractionsController < ApplicationController
+  before_action :set_attraction, only: [:show, :edit, :update]
   def index
     current_user
     @attractions = Attraction.all
@@ -42,7 +43,7 @@ class AttractionsController < ApplicationController
     if current_user.admin == true
       @attraction = Attraction.find_by(id: params[:id])
       @attraction.update(attraction_params)
-      redirect_to attraction_path(attraction)
+      redirect_to attraction_path(@attraction)
     else
       redirect_to attractions_path
     end
@@ -60,5 +61,9 @@ class AttractionsController < ApplicationController
 
   def attraction_params
     params.require(:attraction).permit(:name, :min_height, :nausea_rating, :happiness_rating, :tickets)
+  end
+
+  def set_attraction
+    @attraction = Attraction.find_by(id: params[:id])
   end
 end
