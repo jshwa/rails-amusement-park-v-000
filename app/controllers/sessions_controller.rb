f45e66aaf4060de7aaa0cf96721e65996b01ca6d
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-24 05:45:48 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_by(name: params[:name])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+
   end
 end
