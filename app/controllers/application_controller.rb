cfcc6003b8d8a4c08cb1e971284d3685b467ed3e
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-24 06:48:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
 
   def home
   end
+
+helpers
+  def logged_in?
+    !!current_user
+  end
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
