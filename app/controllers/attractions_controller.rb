029d8ff972947440e455ec65db9bd2d6dcb5df20
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-24 17:03:37 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,8 +4,20 @@ class AttractionsController < ApplicationController
   end
 
   def show
+    @attraction = Attractions.find_by(id: params[:id])
   end
 
   def new
+    @attraction = Attraction.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name)
   end
 end
