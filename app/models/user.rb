95dea0700166b58512327b9f11fbbb6086383bfb
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-24 03:41:42 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys: [:password]
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:password]
 
   has_secure_password
 
